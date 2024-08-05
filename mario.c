b68044850186d157b1d41a197b80d32aa7b8bbc4
Carnitas <mhayes37@proton.me>
2024-08-05 16:09:16 -0600

    tinkering

--- mario.c
+++ mario.c
@@ -7,10 +7,13 @@ void print_space(int height);
 int main(void)
 {
     int n = get_int("Height? ");
-    for (int i = n ; i > 0 ; i--)
+    // I want to decay the number first, 3 > 1
+    for (int i = n; i > 0; i--)
     {
         {
+            // I want spaces that count down 2 spaces #, then 1 space #, then no space
             print_space(i);
+            // I want to print 1 block (see height + 1), then 2, then 3
             print_block(n - i);
         }
     }
@@ -27,7 +30,9 @@ void print_block(int height)
 
 void print_space(int height)
 {
-    for ( int i = height; i > 0; i--)
+    // We remove one from height so the lowest line has no leading space
+    for (int i = height - 1; i > 0; i--)
+    // This for loop is the same as main but I can't figure out how to reduce them
     {
         printf(" ");
     }
