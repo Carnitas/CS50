ed6d64886486aa874a43e8e485fa7cf992c8b2a1
Carnitas <mhayes37@proton.me>
2024-08-07 07:49:16 -0600

    Style check, add comments

--- cash/cash.c
+++ cash/cash.c
@@ -29,38 +29,51 @@ int main(void)
         printf("That's %i quarters\n", get_quarters(n));
         increment_coin(get_quarters(n));
         increment_leftover(n - (count * 25));
-    if (leftover >= 10)
-    {
-        printf("and %i dimes\n", get_dimes(leftover));
-        increment_coin(get_dimes(leftover));
-        increment_leftover(leftover -= (get_dimes(leftover) * 10));
-    }
-    if (leftover >= 5)
-    {
-        printf("and %i nickles\n", get_nickles(leftover));
-        increment_coin(get_nickles(leftover));
-        increment_leftover(leftover -= (get_nickles(leftover) * 5));
-    }
-    if (leftover)
-    {
-        printf("and %i pennies\n", leftover);
-        increment_coin(leftover);
-        increment_leftover(leftover -= leftover);
-    }
-    printf("total coins: %i\n", count);
+        // Here we process the number of dimes based on the remaining amount
+        if (leftover >= 10)
+        {
+            printf("and %i dimes\n", get_dimes(leftover));
+            increment_coin(get_dimes(leftover));
+            increment_leftover(leftover -= (get_dimes(leftover) * 10));
+        }
+        // Here we process nickles, based on the value of leftover
+        if (leftover >= 5)
+        {
+            printf("and %i nickles\n", get_nickles(leftover));
+            increment_coin(get_nickles(leftover));
+            increment_leftover(leftover -= (get_nickles(leftover) * 5));
+        }
+
+        // Given that every operation would be * or / by 1, we just return the leftover value.
+        if (leftover)
+        {
+            printf("and %i pennies\n", leftover);
+            increment_coin(leftover);
+            increment_leftover(leftover -= leftover);
+        }
+
+        printf("total coins: %i\n", count);
     }
 }
 
+// I used an incrementer for coin count outside the function
+// ChatGPT gave me a model where I could just increment in the function
+// which makes more sense because that value won't be referenced outside of main
 void increment_coin(int coin)
 {
     count += coin;
 }
 
+// Same as above. Initially thought I needed an extra-function counter
+// But I could initialize it within main and reduce this complexity.
 void increment_leftover(int change)
 {
     leftover = change;
 }
 
+// The following are helper functions to process coin values
+// We could probably just do the math within main
+// but this facilitated the outer-function counter.
 int get_quarters(int cents)
 {
     return cents / 25;
@@ -76,6 +89,7 @@ int get_nickles(int cents)
     return cents / 5;
 }
 
+// This function is useless but I added it for completeness in design.
 int get_pennies(int cents)
 {
     return cents;
