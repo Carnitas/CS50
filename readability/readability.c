79d1c4a39d87278d8f0d9086d93681d246b13364
Carnitas <mhayes37@proton.me>
2024-08-11 20:08:38 -0600

    Early readability commit

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,19 @@
+#include <ctype.h>
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
+    string text = get_string("Enter text: ");
 
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            printf("%c", text[i]);
+        }
+    }
+    printf("\n");
 }
