88b2e00fcb3382f2a9e9f7b199cbe6f0694f27fc
Carnitas <mhayes37@proton.me>
2024-08-13 12:25:07 -0600

    Able to return one word per newline

--- readability/readability.c
+++ readability/readability.c
@@ -10,22 +10,9 @@ void print_single_word(string text);
 int main(void)
 {
     string text = get_string("Enter text: ");
+    print_single_word(text);
+}
 
-    // for (int i = 0, n = strlen(text); i < n; i++)
-    // {
-        print_single_word(text);
-    // }
-    
-    // for (int i = 0, n = strlen(text); i < n; i++)
-    //     if (isblank(text[i]))
-    //     {
-    //         for (int j = 0; j < i; j++)
-    //         {
-    //             printf("%c", text[j]);
-    //         }
-    //         return false;
-    //     }
-    }
 
 void print_single_word(string text)
 {
@@ -35,10 +22,14 @@ void print_single_word(string text)
         {
             for (int j = 0; j < i; j++)
             {
-                printf("%c", text[j]);
+                printf("%c\n", text[i]);
+                break;
             }
-            break;
         }
-    
+        else if (isalpha(text[i]))
+        {
+                printf("%c", text[i]);
+        }
     }
+    printf("\n");
 }
