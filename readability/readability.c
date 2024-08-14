4fc3de134d334bc2a700153247e2eaae4eae3da7
Carnitas <mhayes37@proton.me>
2024-08-13 22:41:36 -0600

    Figure out last word that isn't followed by a space

--- readability/readability.c
+++ readability/readability.c
@@ -20,21 +20,18 @@ int main(void)
 
 void print_single_word(string text)
 {
+    int starting_index = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isblank(text[i]))
         {
-            for (int j = 0; j < i; j++)
+            for (int j = starting_index; j < i; j++)
             {
-                printf("%c\n", text[i]);
-                word_count++;
-                break;
+                printf("%c", text[j]);
             }
-        }
-        else if (isalpha(text[i]))
-        {
-                printf("%c", text[i]);
+            printf("\n");
+            starting_index = i + 1;
+            word_count++;
         }
     }
-    printf("\n");
 }
