91e6f791b49cd368cb1a3035c36037b019ad03e9
Carnitas <michael.hayes37@gmail.com>
2024-08-14 14:17:39 -0600

    Scrabble revisions (#6)
    * Scrabble revisions
    
    * Scrabble revisions
    
    * Update scrabble/scrabble.c
    
    Co-authored-by: Jonathan Gopel <jgopel@gmail.com>
    
    ---------
    
    Co-authored-by: Carnitas <mhayes37@proton.me>
    Co-authored-by: Jonathan Gopel <jgopel@gmail.com>

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -14,46 +14,29 @@
 
 // Prototypes
 int matched_value(char letter);
-void string_checker(string input);
-
-// Global variables
-// These don't need to be global, but feels more readable to know what's coming.
-int input1_sum = 0;
-int input2_sum = 0;
+bool string_checker(string input);
+int summed_value(string input);
 
 // Helper arrays
-char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+const char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-int values[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+const int values[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
-    // Need two do statements to protect each string
+    // Require alphabetic characters for input
     string input1;
-    do
-    {
-        input1 = get_string("Player 1's word: ");
-        string_checker(input1);
-    }
-    while (strlen(input1) <= 0);
-
     string input2;
     do
     {
+        input1 = get_string("Player 1's word: ");
         input2 = get_string("Player 2's word: ");
-        string_checker(input2);
     }
-    while (strlen(input1) <= 0 && strlen(input1) <= 0);
+    while (string_checker(input1) && string_checker(input2));
 
     // Let's get a sum for each player's word
-    for (int i = 0, n = strlen(input1); i < n; i++)
-    {
-        input1_sum += matched_value(input1[i]);
-    }
-    for (int i = 0, n = strlen(input2); i < n; i++)
-    {
-        input2_sum += matched_value(input2[i]);
-    }
+    const int input1_sum = summed_value(input1);
+    const int input2_sum = summed_value(input2);
     
     // This is really a print debug statement, but I like it
     printf("Player 1's score: %i\n", input1_sum);
@@ -69,37 +52,49 @@ int main(void)
     }
     else
     {
-        printf("Tie!");
+        printf("Tie!\n");
     }
 }
 
 // helper functions
+// Sum the value of a word
+int summed_value(string input)
+{
+    int score = 0;
+    for (size_t i = 0, n = strlen(input); i < n; i++)
+    {
+        score += matched_value(input[i]);
+    }
+    return score;
+}
 // From a character, return its corresponding int value
 int matched_value(char letter)
 {
-    int word_sum = 0;
+    int points_per_letter = 0;
     // n = (26 - 1) is dumb, but there are 26 letters in the alphabet and I didn't
     // want to make an off-by-one error as we went along.
-    for (int i = 0, n = (26 - 1); i < n; i++)
+    for (int i = 0, n = 26; i < n; i++)
     {
         // In order to avoid duplicate arrays, make all characters uppercase
         if (letters[i] == toupper(letter))
         {
-            word_sum += values[i];   
-        }
-    }
-    return word_sum;
-}
-
-// Reject non-alphabetic inputs
-void string_checker(string input)
-{
-    for (int i = 0, n = strlen(input); i < n; i++)
-    {
-        if (isalpha(input[i]) == 0)
-        {
-            printf("Only letters are allowed.\n");
+            points_per_letter += values[i];   
             break;
         }
     }
+    return points_per_letter;
+}
+
+// Reject non-alphabetic inputs
+bool string_checker(string input)
+{
+    for (size_t i = 0, n = strlen(input); i < n; i++)
+    {
+        if (isalpha(input[i]))
+        {
+            printf("Only letters are allowed.\n");
+            return false;
+        }
+    }
+    return true;
 }
