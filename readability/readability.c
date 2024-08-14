54b51a313670b4a9db9d362d0df2105aba425413
Carnitas <mhayes37@proton.me>
2024-08-14 07:52:45 -0600

    Figure out why word_count gets reset to 0

--- readability/readability.c
+++ readability/readability.c
@@ -6,15 +6,28 @@
 
 // Prototypes
 void print_single_word(string text);
+void print_single_sentece(string text);
 
 // Global variables
 int word_count = 0;
+int sentence_count = 0; 
 
 int main(void)
 {
     string text = get_string("Enter text: ");
     print_single_word(text);
     printf("Total words: %i\n", word_count);
+    print_single_sentece(text);
+    printf("Total sentences: %i\n", sentence_count);
+
+    float L = word_count/100;
+    float S = sentence_count/L;
+    printf("%f", L);
+
+    float index = 0;
+    index = 0.0588 * L - 0.296 * S - 15.8;
+    printf("Grade level: %f\n", index);
+    
 }
 
 
@@ -23,6 +36,7 @@ void print_single_word(string text)
     int starting_index = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
+        
         if (isspace(text[i]))
         {
             for (int j = starting_index; j < i; j++)
@@ -32,6 +46,36 @@ void print_single_word(string text)
             printf("\n");
             starting_index = i + 1;
             word_count++;
+        }
+    }
+    if (strrchr(text, '.'))
+    {
+        for (int i = starting_index, n = strlen(text); i < n; i++)
+        {
+            printf("%c", text[i]);
+        }
+        word_count++;
+            
+            
+    }
+    printf("\n");
+    // word_count++;
+}
+
+void print_single_sentece(string text)
+{
+    int starting_index = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (ispunct(text[i]) && (text[i] != ',') && (text[i] != '\''))
+        {
+            for (int j = starting_index; j < i; j++)
+            {
+                printf("%c", text[j]);
+            }
+            printf("\n");
+            starting_index = i + 1;
+            sentence_count++;
         }
     }
 }
