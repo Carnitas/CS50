9bc883613ee34e66a7206870e87088cca8e31b4b
Carnitas <michael.hayes37@gmail.com>
2024-08-15 14:06:24 -0600

    Initial commit for readability (#5)
    * Initial commit for readability
    
    * Bin removal
    
    * Early readability commit
    
    * Able to return one word in a sentence
    
    * Abstract print_single_word func
    
    * Abstract print_single_word func
    
    * Able to return one word per newline
    
    * Add wordcount global variable
    
    * Figure out last word that isn't followed by a space
    
    * Figure out last word that isn't followed by a space
    
    * Figure out last word that isn't followed by a space
    
    * Figure out why word_count gets reset to 0
    
    * Gotta refine how I parse things
    
    * Passing all the tests
    
    * Dodgy typecasting
    
    * Move global variables to main
    
    * Comment print debugging
    
    * Remove punctation exclusion repetition
    
    * Remove punctation exclusion repetition
    
    * Remove punctation exclusion repetition, replace with simpler check
    
    * First revisions
    
    * Update readability/readability.c
    
    Co-authored-by: Jonathan Gopel <jgopel@gmail.com>
    
    * Update readability/readability.c
    
    Co-authored-by: Jonathan Gopel <jgopel@gmail.com>
    
    ---------
    
    Co-authored-by: Carnitas <mhayes37@proton.me>
    Co-authored-by: Jonathan Gopel <jgopel@gmail.com>

---
+++ readability/readability.c
@@ -0,0 +1,117 @@
+// This program takes input, found (here)[https://cs50.harvard.edu/x/2024/psets/2/readability/#how-to-test]
+// and returns an average reading level.
+
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// Prototypes
+float count_single_letters(string text, float letter_count);
+float count_single_words(string text, float word_count);
+float count_sentences(string text, float sentence_count);
+
+int main(void)
+{
+    const string text = get_string("Enter text: ");
+    
+    float letter_count = count_single_letters(text, 0);
+    float word_count = count_single_words(text, 0);
+    float sentence_count = count_sentences(text, 0);
+    
+    // Kept these to track how I printed characters
+    // printf("Total letters: %f\n", letter_count);
+    // printf("Total words: %f\n", word_count); 
+    // printf("Total sentences: %f\n", sentence_count);
+
+    const float L = letter_count/word_count * 100;
+    const float S = sentence_count/word_count * 100;
+
+    const double index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 15)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %.0f\n", round(index));
+    }
+}
+
+// Count alphabetic characters
+float count_single_letters(string text, float letter_count)
+{
+    const size_t num_chars = strlen(text);
+    for (size_t i = 0; i < num_chars; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letter_count++;
+        }
+    }
+    return letter_count;
+}
+
+// Count words
+float count_single_words(string text, float word_count)
+{
+    // The index stuff was for printing individual words so that I could count
+    // from the space to the next space.
+
+    // int starting_index = 0;
+    for (int i = 0, n = (int) strlen(text); i < n; i++)
+    {
+
+        if (isspace(text[i]))
+        {
+            // Just for printing out the words. Only really need the count. 
+            // for (int j = starting_index; j < i; j++)
+            // {
+            //     printf("%c", text[j]);
+            // }
+            // printf("\n");
+            // starting_index = i + 1;
+            word_count++;
+        }
+    }
+    // Since I'm just counting up to spaces, needed to catch the last word.
+    if (strrchr(text, '.'))
+    {
+        // More print debugging.
+        // for (int i = starting_index, n = strlen(text); i < n; i++)
+        // {
+        //     printf("%c", text[i]);
+        // }
+        word_count++;
+    }
+    return word_count;
+}
+
+float count_sentences(string text)
+{
+    // Same as above, promote the starting index to one after a period character.
+    // int starting_index = 0;
+    float sentence_count = 0.0;
+    for (int i = 0, n = (int) strlen(text); i < n; i++)
+    {
+        // Check the ways a sentence can end in English syntax.
+        if (strchr(".!?", text[i]))
+        {
+            // print debugging
+            // for (int j = starting_index; j < i; j++)
+            // {
+            //     printf("%c", text[j]);
+            // }
+            // printf("\n");
+            // starting_index = i + 1;
+            sentence_count++;
+        }
+    }
+    return sentence_count;
+}
