923ae78ec4c892bd9acb6655f134570d7bcd2e89
Carnitas <mhayes37@proton.me>
2024-08-17 14:27:07 -0600

    Works, move into revisions

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,31 +5,35 @@
 #include <string.h>
 
 // Prototypes
-void cipher_text_rotation(string plaintext);
-int compute_rotation_value(int key_value);
+void cipher_text_rotation(string plaintext, int rotation_value);
+// int compute_rotation_value(int key_value);
+void lower_case_rotation(int c, int key_value);
 bool only_digits(string);
+void upper_case_rotation(int c, int key_value);
 
 int main(int argc, string argv[])
 {
     const string key = argv[1];
     if (argc != 2)
     {
-        printf("Usage: ./caesar [number]\n");
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    if (!only_digits(key))
+    {
+        printf("Usage: ./caesar key\n");
         return 1;
     }
 
     const int key_value = atoi(argv[1]);
-    const int rotation_value = compute_rotation_value(key_value);
-    if (only_digits(key))
-    {
-        printf("Integer from key: %i\n", key_value);
-        printf("Rotation value: %i\n", rotation_value);
-    }
-
+    const int rotation_value = key_value % 26;
     const string plaintext = get_string("plaintext:  ");
 
+
+
     printf("ciphertext: ");
-    cipher_text_rotation(plaintext);
+    cipher_text_rotation(plaintext, rotation_value);
 }
 
 // Required by the problem set
@@ -38,39 +42,69 @@ bool only_digits(string key)
     const size_t n = strlen(key);
     for (size_t i = 0; i < n; i++)
     {
-        if (!isdigit(key[i]))
+        if (isdigit(key[i]) == 0)
         {
-            printf("Please try again and enter one number.\n");
             return false;
         }
     }
     return true;
 }
 
-int compute_rotation_value(int key_value)
-{
-    const int rotation_value = key_value % 26;
-    return rotation_value;
-}
+// I might be able to fold this in somewhere
+// int compute_rotation_value(int key_value)
+// {
+//     const int rotation_value = key_value % 26;
+//     return rotation_value;
+// }
 
-void cipher_text_rotation(string plaintext)
+// Here's where we determine which rotation to apply based on character number
+void cipher_text_rotation(string plaintext, int rotation_value)
 {
     const size_t n = strlen(plaintext);
     int c = 0;
     for (size_t i = 0; i < n; i++)
     {
         c = (plaintext[i] - 0);
-        // int rotate_ciphertext = (c + 26);
-        if (c > 90)
+        if (65 <= c && c <= 90)
         {
-            int loop_around = (c % 26) + 64;
-            printf("%c", loop_around);
-            
+            upper_case_rotation(c, rotation_value);
+        }
+        else if ( 97 <= c && c <= 122)
+        {
+            lower_case_rotation(c, rotation_value);
         }
         else
         {
-            printf("%c", c);   
-        } 
+            printf("%c", c);
+        }
     }
     printf("\n");
+}
+
+// Here's what happens for uppercase
+void upper_case_rotation(int c, int rotation_value)
+{
+    int upper_cipher = c + rotation_value;
+    if (upper_cipher > 90)
+    {
+        printf("%c", (upper_cipher - 90) + 64);
+    }
+    else
+    {
+        printf("%c", upper_cipher);
+    }
+}
+
+// Here's what happens for lowercase
+void lower_case_rotation(int c, int rotation_value)
+{
+    int lower_cipher = c + rotation_value;
+    if (lower_cipher > 122)
+    {
+        printf("%c", (lower_cipher - 122) + 96);
+    }
+    else
+    {
+        printf("%c", lower_cipher);
+    }
 }
