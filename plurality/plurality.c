52545db9392d54fe50f06011f4c1b13b5acd9e7c
Carnitas <michael.hayes37@gmail.com>
2024-08-22 07:24:30 -0600

    Designated initialization
    Co-authored-by: Jonathan Gopel <jgopel@gmail.com>

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,7 +81,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    candidate winner = {"", 0};
+    candidate winner = {.name="", .votes=0};
     for (int i = 0; i < candidate_count - 1; i++)
     {
         for (int j = 0; j < candidate_count - 1; j++)
