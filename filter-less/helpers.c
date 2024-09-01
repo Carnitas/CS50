71c2b75ece2250ac24bd922d5cd21126fa2e2df7
Carnitas <mhayes37@proton.me>
2024-09-01 11:59:40 -0600

    Initial commit

---
+++ filter-less/helpers.c
@@ -0,0 +1,34 @@
+#include "helpers.h"
+#include <stdio.h>
+#include <unistd.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            printf("pixel!\n");
+        }
+    }
+
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
