2bad4964e23e6f710b18cca1425784157d346e01
Carnitas <mhayes37@proton.me>
2024-09-01 18:00:50 -0600

    Grayscale filter working

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,16 +1,28 @@
 #include "helpers.h"
+#include <math.h>
 #include <stdio.h>
 #include <unistd.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int gray_value = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // Gray is what happens when all rgb values match.
-            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed;
+            // Gather rgb values and average them
+            int rgbt_sum = 0;
+
+            rgbt_sum += image[i][j].rgbtRed;
+            rgbt_sum += image[i][j].rgbtGreen;
+            rgbt_sum += image[i][j].rgbtBlue;
+
+            uint8_t rgbt_avg = (rgbt_sum + 1)/ 3;
+
+            image[i][j].rgbtRed = rgbt_avg;
+            image[i][j].rgbtGreen = rgbt_avg;
+            image[i][j].rgbtBlue = rgbt_avg;
         }
     }
 }
