477da9382a3b34a2a9bb1d00f470e0be99d827c9
Carnitas <mhayes37@proton.me>
2024-09-03 11:30:29 -0600

    Finished the sepia filter

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -45,6 +45,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             // Create a ptr to access the components of RGBTRIPLE as an array
             // There's a way to do all of this with less repeated code
             // Let's come back to it.
+            // On the other hand, ptr[0] sends you back to bmp.h to figure out
+            // what BYTE it references.
             BYTE* ptr = (BYTE*)&image[i][j];
 
             // sb: sepia blue, sg: sepia green, sr: sepia red
@@ -53,6 +55,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float sr = (0.189 * ptr[0]) + (0.769 * ptr[1]) + (0.393 * ptr[2]);
 
             // lb: leveled blue, lg: leveled green, lr: leveled red
+            // You could probably do this with list comprehension
+            // but no idea in C.
             BYTE lb = (sb > 255) ? 255 : (sb < 0) ? 0 : round(sb);
             BYTE lg = (sg > 255) ? 255 : (sg < 0) ? 0 : round(sg);
             BYTE lr = (sr > 255) ? 255 : (sr < 0) ? 0 : round(sr);
@@ -67,11 +71,45 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // The below commented code flips the image pixel-by-pixel
+    // because there's no need to flip the image BYTE by BYTE.
+    // However, this approach fails the tests, so let's move on.
+
+    // RGBTRIPLE *tmp_ptr = (RGBTRIPLE*)&image[height][width];
+    // for (int i = 0; i < height; i++)
+    // {
+    //     // We need to stop half way through or we flip
+    //     // the flipped pixels back
+    //     for (int j = 0; j < width/2; j++)
+    //     {
+    //         tmp_ptr = (RGBTRIPLE*)&image[i][j];
+    //         image[i][j - 1] = image[i][width - j - 1];
+    //         image[i][width - j - 1] = *tmp_ptr;
+    //     }
+    // }
+
+    BYTE *tmp_byte = (BYTE*)&image[height][width];
     for (int i = 0; i < height; i++)
     {
-        for (int j = width; j > 0; j--)
+        // We need to stop half way through or we flip
+        // the flipped pixels back
+        for (int j = 0; j < width/2; j++)
         {
-            return;
+            // I don't understand why I can't reference tmp_byte.rgbtBlue
+            // Understanding that I'm referencing each BYTE as if in an array
+            // I wonder why I can't reference them by name rather than index
+            tmp_byte[0] = image[i][j].rgbtBlue;
+            tmp_byte[1] = image[i][j].rgbtGreen;
+            tmp_byte[2] = image[i][j].rgbtRed;
+
+            // We subtract one to avoid going out-of-bounds in the swap
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+
+            image[i][width - j - 1].rgbtBlue = tmp_byte[0];
+            image[i][width - j - 1].rgbtGreen = tmp_byte[1];
+            image[i][width - j - 1].rgbtRed = tmp_byte[2];
         }
     }
 }
