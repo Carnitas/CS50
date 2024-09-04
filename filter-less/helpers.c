832cc74e5e1e467acfa0b9d99a915d773fb90e77
Carnitas <mhayes37@proton.me>
2024-09-04 11:51:35 -0600

    Got a blur, values off

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,6 +22,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             }
 
             // Average those values and reassign the single value to each BYTE of that pixel
+            // +1 to round properly
             uint8_t rgbt_avg = (rgbt_sum + 1)/ 3;
 
             for (int k = 0; k < sizeof(RGBTRIPLE); k++)
@@ -117,5 +118,43 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Create a ptr to access the components of RGBTRIPLE as an array
+            BYTE* ptr = (BYTE*)&image[i][j];
+            RGBTRIPLE *copy = &image[i][j];
+            int rgbt_sum_surrounding_blue = 0;
+            int rgbt_sum_surrounding_green = 0;
+            int rgbt_sum_surrounding_red = 0;
+            int valid_pixel_counter = 0;
+
+            // Gather the values in each BYTE and sum them
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+
+                    int valid_i = (i + di >= 0 && i + di < height) ? i + di : -1;
+                    int valid_j = (j + dj >= 0 && j + dj < width) ? j + dj : -1;
+                    if (valid_i != -1 && valid_j != -1)
+                    {
+                        rgbt_sum_surrounding_blue  += image[valid_i][valid_j].rgbtBlue;
+                        rgbt_sum_surrounding_green += image[valid_i][valid_j].rgbtGreen;
+                        rgbt_sum_surrounding_red   += image[valid_i][valid_j].rgbtRed;
+                        valid_pixel_counter++;
+
+                    }
+                }
+            }
+            image[i][j].rgbtBlue = (rgbt_sum_surrounding_blue + 1)/ valid_pixel_counter;
+            image[i][j].rgbtGreen = (rgbt_sum_surrounding_green + 1)/ valid_pixel_counter;
+            image[i][j].rgbtRed = (rgbt_sum_surrounding_red + 1)/ valid_pixel_counter;
+            // rgbt_sum_surrounding_blue = 0;
+            // rgbt_sum_surrounding_green = 0;
+            // rgbt_sum_surrounding_red = 0;
+            // valid_pixel_counter = 0;
+        }
+    }
 }
