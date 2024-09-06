dd0dfd8989f4d91ecb588c322ae46ff60c58208b
Carnitas <mhayes37@proton.me>
2024-09-06 10:54:59 -0600

    No real changes to blur

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,7 +5,6 @@
 
 // Prototypes
 
-RGBTRIPLE calculate_blur(RGBTRIPLE pixel);
 RGBTRIPLE calculate_sepia(RGBTRIPLE pixel);
 uint8_t calculate_grayscale(RGBTRIPLE pixel);
 void copy_image(int height, int width, RGBTRIPLE src[height][width], RGBTRIPLE dest[height][width]);
@@ -73,22 +72,34 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // For any pixel within one square of the pixel
-            // average its byte values
+            int rgbt_sum_surrounding_blue = 0;
+            int rgbt_sum_surrounding_green = 0;
+            int rgbt_sum_surrounding_red = 0;
+            float valid_pixel_counter = 0;
+
+            // For any pixel within one square of the pixel average its byte values
+
             for (int di = -1; di <= 1; di++)
             {
                 for (int dj = -1; dj <= 1; dj++)
                 {
 
-                    int valid_i = (i + di >= 0 && i + di < height) ? i + di : -1;
-                    int valid_j = (j + dj >= 0 && j + dj < width) ? j + dj : -1;
-                    if (valid_i != -1 && valid_j != -1)
+                    int valid_i = i + di;
+                    int valid_j = j + dj;
+                    if ((height > valid_i && valid_i >=0) && (width > valid_j && valid_j >= 0))
                     {
-                        temp_blur[i][j] = calculate_blur(copy[valid_i][valid_j]);
+                        rgbt_sum_surrounding_blue  += copy[valid_i][valid_j].rgbtBlue;
+                        rgbt_sum_surrounding_green += copy[valid_i][valid_j].rgbtGreen;
+                        rgbt_sum_surrounding_red   += copy[valid_i][valid_j].rgbtRed;
+                        valid_pixel_counter++;
+
                     }
                 }
             }
-            image[i][j] = temp_blur[i][j];
+            // Once we've got the average bytes, let's rebuild the image array with those values
+            image[i][j].rgbtBlue = round((rgbt_sum_surrounding_blue / valid_pixel_counter);
+            image[i][j].rgbtGreen = round((rgbt_sum_surrounding_green / valid_pixel_counter);
+            image[i][j].rgbtRed = round((rgbt_sum_surrounding_red / valid_pixel_counter);
         }
     }
 }
@@ -127,24 +138,4 @@ RGBTRIPLE calculate_sepia(RGBTRIPLE pixel)
     sepia_pixel.rgbtRed = (sr > 255) ? 255 : (sr < 0) ? 0 : round(sr);
 
     return sepia_pixel;
-}
-
-RGBTRIPLE calculate_blur(RGBTRIPLE pixel)
-{
-    int rgbt_sum_surrounding_blue = 0;
-    int rgbt_sum_surrounding_green = 0;
-    int rgbt_sum_surrounding_red = 0;
-    float valid_pixel_counter = 0;
-
-    rgbt_sum_surrounding_blue  += pixel.rgbtBlue;
-    rgbt_sum_surrounding_green += pixel.rgbtGreen;
-    rgbt_sum_surrounding_red   += pixel.rgbtRed;
-    valid_pixel_counter++;
-
-    RGBTRIPLE blur_pixel;
-    blur_pixel.rgbtBlue = round((rgbt_sum_surrounding_blue) / valid_pixel_counter);
-    blur_pixel.rgbtGreen = round((rgbt_sum_surrounding_green)/ valid_pixel_counter);
-    blur_pixel.rgbtRed = round((rgbt_sum_surrounding_red)/ valid_pixel_counter);
-
-    return blur_pixel;
 }
