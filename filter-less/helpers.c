8281cbd9e94392e89a8a022c27feecc0afea95b1
Carnitas <mhayes37@proton.me>
2024-09-06 08:40:06 -0600

    Revise reflect and sepia

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,7 +4,11 @@
 #include <unistd.h>
 
 // Prototypes
+
+RGBTRIPLE calculate_blur(RGBTRIPLE pixel);
+RGBTRIPLE calculate_sepia(RGBTRIPLE pixel);
 uint8_t calculate_grayscale(RGBTRIPLE pixel);
+void copy_image(int height, int width, RGBTRIPLE src[height][width], RGBTRIPLE dest[height][width]);
 
 // Convert image to grayscale
 // Gray is what happens when all rgb values are the same on a given pixel
@@ -14,9 +18,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Create a ptr to access the components of RGBTRIPLE as an array
-            // BYTE* ptr = (BYTE*)&image[i][j];
-
             uint8_t rgbt_avg = calculate_grayscale(image[i][j]);
             image[i][j].rgbtBlue = rgbt_avg;
             image[i][j].rgbtGreen = rgbt_avg;
@@ -25,62 +26,37 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     }
 }
 
-// Convert image to sepia
-// sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
-// sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
-// sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+// Convert image to sepi
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    copy_image(height, width, image, copy);
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            BYTE* ptr = (BYTE*)&image[i][j];
-
-            // sb: sepia blue, sg: sepia green, sr: sepia red
-            float sb = (0.131 * ptr[0]) + (0.534 * ptr[1]) + (0.272 * ptr[2]);
-            float sg = (0.168 * ptr[0]) + (0.686 * ptr[1]) + (0.349 * ptr[2]);
-            float sr = (0.189 * ptr[0]) + (0.769 * ptr[1]) + (0.393 * ptr[2]);
-
-            // lb: leveled blue, lg: leveled green, lr: leveled red
-            // You could probably do this with list comprehension
-            // but no idea in C.
-            BYTE lb = (sb > 255) ? 255 : (sb < 0) ? 0 : round(sb);
-            BYTE lg = (sg > 255) ? 255 : (sg < 0) ? 0 : round(sg);
-            BYTE lr = (sr > 255) ? 255 : (sr < 0) ? 0 : round(sr);
-
-            ptr[0] = lb;
-            ptr[1] = lg;
-            ptr[2] = lr;
+            image[i][j] = calculate_sepia(copy[i][j]);
         }
+
     }
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    BYTE *tmp_byte = (BYTE*)&image[height][width];
+    // Copy image to copy
+    RGBTRIPLE copy[height][width];
+    copy_image(height, width, image, copy);
     for (int i = 0; i < height; i++)
     {
-        // We need to stop half way through or we flip
-        // the flipped pixels back
+        // We need to stop half way through or we flip the flipped pixels back
         for (int j = 0; j < width/2; j++)
         {
-            // I don't understand why I can't reference tmp_byte.rgbtBlue
-            // Understanding that I'm referencing each BYTE as if in an array
-            // I wonder why I can't reference them by name rather than index
-            tmp_byte[0] = image[i][j].rgbtBlue;
-            tmp_byte[1] = image[i][j].rgbtGreen;
-            tmp_byte[2] = image[i][j].rgbtRed;
-
             // We subtract one to avoid going out-of-bounds in the swap
-            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
-            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
-            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j]= copy[i][width - j - 1];
 
-            image[i][width - j - 1].rgbtBlue = tmp_byte[0];
-            image[i][width - j - 1].rgbtGreen = tmp_byte[1];
-            image[i][width - j - 1].rgbtRed = tmp_byte[2];
+            // Inverse pixel receives the original pixel
+            image[i][width - j - 1] = copy[i][j];
         }
     }
 }
@@ -89,25 +65,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy[height][width];
-    // Let's make a copy of the image array, allows us to reset copy too.
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            copy[i][j] = image[i][j];
-        }
-    }
+    copy_image(height, width, image, copy);
+    RGBTRIPLE temp_blur[height][width];
 
     // For each rgbtriple in the image array, let's average its bytes.
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            int rgbt_sum_surrounding_blue = 0;
-            int rgbt_sum_surrounding_green = 0;
-            int rgbt_sum_surrounding_red = 0;
-            float valid_pixel_counter = 0;
-
             // For any pixel within one square of the pixel
             // average its byte values
             for (int di = -1; di <= 1; di++)
@@ -119,26 +84,67 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     int valid_j = (j + dj >= 0 && j + dj < width) ? j + dj : -1;
                     if (valid_i != -1 && valid_j != -1)
                     {
-                        rgbt_sum_surrounding_blue  += copy[valid_i][valid_j].rgbtBlue;
-                        rgbt_sum_surrounding_green += copy[valid_i][valid_j].rgbtGreen;
-                        rgbt_sum_surrounding_red   += copy[valid_i][valid_j].rgbtRed;
-                        valid_pixel_counter++;
-
+                        temp_blur[i][j] = calculate_blur(copy[valid_i][valid_j]);
                     }
                 }
             }
-            // Once we've got the average bytes, let's rebuild the image array with those values
-            image[i][j].rgbtBlue = round((rgbt_sum_surrounding_blue) / valid_pixel_counter);
-            image[i][j].rgbtGreen = round((rgbt_sum_surrounding_green)/ valid_pixel_counter);
-            image[i][j].rgbtRed = round((rgbt_sum_surrounding_red)/ valid_pixel_counter);
+            image[i][j] = temp_blur[i][j];
         }
     }
 }
 
 // helper functions
+void copy_image(int height, int width, RGBTRIPLE src[height][width], RGBTRIPLE dest[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            dest[i][j] = src[i][j];
+        }
+    }
+}
+
 uint8_t calculate_grayscale(RGBTRIPLE pixel)
 {
     int rgbt_sum = pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed;
     // +1 to round properly
     return (rgbt_sum + 1) / 3;
+}
+
+// sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+// sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+// sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+RGBTRIPLE calculate_sepia(RGBTRIPLE pixel)
+{
+    float sb = (0.131 * pixel.rgbtBlue) + (0.534 * pixel.rgbtGreen) + (0.272 * pixel.rgbtRed);
+    float sg = (0.168 * pixel.rgbtBlue) + (0.686 * pixel.rgbtGreen) + (0.349 * pixel.rgbtRed);
+    float sr = (0.189 * pixel.rgbtBlue) + (0.769 * pixel.rgbtGreen) + (0.393 * pixel.rgbtRed);
+
+    RGBTRIPLE sepia_pixel;
+    sepia_pixel.rgbtBlue = (sb > 255) ? 255 : (sb < 0) ? 0 : round(sb);
+    sepia_pixel.rgbtGreen = (sg > 255) ? 255 : (sg < 0) ? 0 : round(sg);
+    sepia_pixel.rgbtRed = (sr > 255) ? 255 : (sr < 0) ? 0 : round(sr);
+
+    return sepia_pixel;
+}
+
+RGBTRIPLE calculate_blur(RGBTRIPLE pixel)
+{
+    int rgbt_sum_surrounding_blue = 0;
+    int rgbt_sum_surrounding_green = 0;
+    int rgbt_sum_surrounding_red = 0;
+    float valid_pixel_counter = 0;
+
+    rgbt_sum_surrounding_blue  += pixel.rgbtBlue;
+    rgbt_sum_surrounding_green += pixel.rgbtGreen;
+    rgbt_sum_surrounding_red   += pixel.rgbtRed;
+    valid_pixel_counter++;
+
+    RGBTRIPLE blur_pixel;
+    blur_pixel.rgbtBlue = round((rgbt_sum_surrounding_blue) / valid_pixel_counter);
+    blur_pixel.rgbtGreen = round((rgbt_sum_surrounding_green)/ valid_pixel_counter);
+    blur_pixel.rgbtRed = round((rgbt_sum_surrounding_red)/ valid_pixel_counter);
+
+    return blur_pixel;
 }
