12e3fceb4df9b129a44bf5a3cdcee0a2d69be1b2
Carnitas <mhayes37@proton.me>
2024-09-09 07:20:17 -0600

    Identifying buffer starts

--- recover/recover.c
+++ recover/recover.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-
 // int main(int argc, char *argv[])
 // {
 
@@ -15,30 +12,39 @@
 //     // Create JPEGs from the data
 // }
 
+// Variables
+typedef uint8_t BYTE;
+
 
 int main(int argc, char *argv[])
 {
     // Check for invalid usage
-    if (argc < 2)
+    if (argc != 2)
     {
-        printf("Usage: Enter file path for recoverable image.\n");
+        printf("Usage: ./recover [filepath]\n");
         return 1;
     }
 
-    // Remember filenames
-    char *infile = argv[optind];
+    char *infile = argv[1];
 
-    // Attempt to open file
-    FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
+    FILE *card = fopen(infile, "r");
+    if (card == NULL)
     {
         printf("Could not open %s.\n", infile);
-        return 4;
+        return 2;
     }
-    else
+
+    // Create a buffer
+    uint8_t buffer[512];
+
+
+    while (fread(&buffer, 512, 1, card))
     {
-        printf("File opened!\n");
-        fclose(inptr);
-        return 0;
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        {
+            printf("Possible buffer start\n");
+        }
     }
+
+    fclose(card);
 }
