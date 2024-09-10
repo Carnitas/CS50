8afc1ac137295cc05954f8d2e5f3c471cd5807b5
Carnitas <mhayes37@proton.me>
2024-09-10 08:48:21 -0600

    First helper function

--- recover/recover.c
+++ recover/recover.c
@@ -15,6 +15,9 @@
 // Variables
 typedef uint8_t BYTE;
 
+// Prototypes
+void possible_image_start(uint8_t buffer[512]);
+
 
 int main(int argc, char *argv[])
 {
@@ -34,17 +37,26 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    // Create a buffer
+    // Create a buffer to look for jpeg signatures
     uint8_t buffer[512];
 
 
     while (fread(&buffer, 512, 1, card))
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
-        {
-            printf("Possible buffer start\n");
-        }
+        possible_image_start(buffer);
     }
 
     fclose(card);
+}
+
+
+// Helper functions
+
+// Identify possible value clusters to indicate jpeg.
+void possible_image_start(uint8_t buffer[512])
+{
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+    {
+        printf("Possible image start\n");
+    }
 }
