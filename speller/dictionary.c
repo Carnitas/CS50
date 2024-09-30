46f0bef36dc52b021efcec2cfad20af8ba8bf92e
Carnitas <mhayes37@proton.me>
2024-09-30 08:37:53 -0600

    Revisions

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -9,12 +9,20 @@
 #include "dictionary.h"
 
 // Represents a node in a hash table
+// We're padding the length of the word to 56 bytes with another 8 bytes
+// held for the *next register to better align with cache lines.
+// No substantial performance improvement but a good reference.
 typedef struct node
 {
-    char word[LENGTH + 1];
+    char word[LENGTH + 11];
     struct node *next;
 } node;
 
+
+// The size of the dictionary is 143091 entries. I chose to round up to 150,000
+// for readability and created a container with a density of 0.75 (150000 / 0.75)
+// or 200000. Then, rounded it to the nearest prime, which can generally improve
+// hashing function performance.
 const unsigned int N = 200007;
 
 // Hash table
@@ -26,7 +34,7 @@ int dict_word_count = 0;
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    const int x = hash(word) % N;
+    const int x = hash(word);
     return (table[x] != NULL && strcasecmp(table[x]->word, word) == 0);
 }
 
@@ -38,18 +46,24 @@ unsigned int hash(const char *word)
     {
         ascii_sum += (int)tolower(word[i]);
     }
-    return ascii_sum;
+    return ascii_sum % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    FILE *source = fopen(dictionary, "r");
     char buffer[LENGTH + 1];
+    node *n = NULL;
+    FILE *source = fopen(dictionary, "r");
+    if (source == NULL)
+    {
+        printf("Could not open dictionary.");
+        return 1;
+    }
 
     while (fscanf(source, "%s", buffer) != EOF)
     {
-        node *n = malloc(sizeof(node));
+        n = malloc(sizeof(node));
 
         strcpy(n->word, buffer);
         n->next = NULL;
