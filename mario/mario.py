1bffc49dd43dc5bd6aa807c05a59b82b82860c8f
Carnitas <mhayes37@proton.me>
2024-10-01 08:47:22 -0600

    First draft

--- mario/mario.py
+++ mario/mario.py
@@ -1,7 +1,26 @@
 from cs50 import get_int
 
-rows = get_int("Number of rows: ")
-for i in range(rows):
-    for j in range(rows):
+height = get_int("Number of rows: ")
+
+
+def main():
+    x = height
+    while x > 0:
+        print_space(x)
+        print_blocks(height - x)
+        x -= 1
+
+
+def print_blocks(blocks):
+    for i in range(blocks + 1):
         print("#", end="")
     print("")
+
+
+def print_space(space):
+    while space > 0:
+        print(" ", end="")
+        space -= 1
+
+
+main()
