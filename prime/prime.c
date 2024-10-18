853cb1bac1df65bc8d3b63a86ad70a8d5e749352
Carnitas <mhayes37@proton.me>
2024-10-18 07:59:31 -0600

    Can't figure out primes lower than 10

---
+++ prime/prime.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    }
+    while (min >= max);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+    // We'll throw out 1 and all even numbers, then we only need to
+    // check for 3-9 for
+    for (int i = 3; i < 10; i++)
+    {
+        if ((number % 2) == 0 || (number % i == 0))
+        {
+            return false;
+        }
+    }
+    return true;
+}
