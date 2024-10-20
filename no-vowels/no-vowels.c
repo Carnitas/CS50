4c1a88dc28f0b83b943b34e619be522bc935d08b
Carnitas <mhayes37@proton.me>
2024-10-20 08:03:00 -0600

    No Vowels problem

---
+++ no-vowels/no-vowels.c
@@ -0,0 +1,52 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <cs50.h>
+#include <stdio.h>
+
+// Prototype
+void replace(char*);
+
+typedef struct {
+    char letter;
+    char symbol;
+} leet_map;
+
+leet_map map[8] = {
+    {'A', '6'},
+    {'a', '6'},
+    {'E', '3'},
+    {'e', '3'},
+    {'I', '1'},
+    {'i', '1'},
+    {'O', '0'},
+    {'o', '0'},
+};
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("usage: ./no-vowels word\n");
+        return 1;
+    }
+    replace(argv[1]);
+}
+
+void replace(char* word)
+{
+    const int map_size = sizeof(map) / sizeof(map[0]);
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+       for (int j = 0; j < map_size; j++)
+        {
+            if (word[i] == map[j].letter)
+            {
+                word[i] = map[j].symbol;
+            }
+        }
+    }
+    printf("%s", word);
+}
