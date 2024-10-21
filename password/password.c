fb1f677ce1c16cbf2ecb38779ffde1a4e99d2612
Carnitas <mhayes37@proton.me>
2024-10-21 16:58:25 -0600

    Password problem

--- password/password.c
+++ password/password.c
@@ -4,6 +4,7 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
 bool valid(string password);
 
@@ -23,5 +24,33 @@ int main(void)
 // TODO: Complete the Boolean function below
 bool valid(string password)
 {
+    bool uppercase_found = false;
+    bool lowercase_found = false;
+    bool number_found = false;
+    bool symbol_found = false;
+
+    for (int i = 0; i < sizeof(password); i++)
+    {
+        if (isupper(password[i]))
+        {
+            uppercase_found = true;
+        }
+        else if (islower(password[i]))
+        {
+            lowercase_found = true;
+        }
+        else if (isdigit(password[i]))
+        {
+            number_found = true;
+        }
+        else if (ispunct(password[i]))
+        {
+            symbol_found = true;
+        }
+    }
+    if (uppercase_found && lowercase_found && number_found && symbol_found)
+    {
+        return true;
+    }
     return false;
 }
