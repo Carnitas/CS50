74b4b71819fc468ed1fa5593011f04248f4b6f8c
Carnitas <mhayes37@proton.me>
2024-10-23 08:21:45 -0600

    style fixes

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -25,12 +25,16 @@ int main(void)
 
 int convert(string input)
 {
+    // Base case. Will fire if the only character in the string is the null character.
     if (strlen(input) == 0)
     {
         return 0;
     }
+    // The ascii number for the numeral zero is '48', so let's subtract that to get to our 0 integer.
     int holder = input[strlen(input) - 1] - 48;
+    // Move the null character one closer to the input start.
     input[strlen(input) - 1] = '\0';
+    // Add the first value to 10 times the remaining string. I don't fully understand this.
     holder += convert(input) * 10;
     return holder;
 }
