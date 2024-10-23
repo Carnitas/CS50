0fa42f1be0c5338e0c3bafb98caab5ade16d7a7c
Carnitas <mhayes37@proton.me>
2024-10-23 08:14:37 -0600

    Got it in two hours

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -25,14 +25,13 @@ int main(void)
 
 int convert(string input)
 {
-    int converted_number = 0;
+
     if (strlen(input) == 0)
     {
         return 0;
     }
-    for (int i = strlen(input); i != '\0'; i--)
-    {
-        return input[i - 1] - 48;
-    }
-    return converted_number;
+    int holder = input[strlen(input) - 1] - 48;
+    input[strlen(input) - 1] = '\0';
+    holder += convert(input) * 10;
+    return holder;
 }
